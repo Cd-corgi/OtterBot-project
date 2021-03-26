141efbc79143891e9fd8f7bffdf7e96feeaf561b
Cd-corgi <70237066+Cd-corgi@users.noreply.github.com>
2021-03-26 11:47:10 -0500

    updated message

--- OtterBot V1.0.0.0/Bot/events/message.js
+++ OtterBot V1.0.0.0/Bot/events/message.js
@@ -1,38 +1,13 @@
-const mongoose = require('mongoose');
-const Guild = require('../models/guild');
-
 module.exports = async (client, message) => {
     if (message.author.bot) return;
 
 		
 
-    const settings = await Guild.findOne({
-        guildID: message.guild.id
-    }, (err, guild) => {
-        if (err) console.error(err)
-        if (!guild) {
-            const newGuild = new Guild({
-                _id: mongoose.Types.ObjectId(),
-                guildID: message.guild.id,
-                guildName: message.guild.name,
-                prefix: process.env.PREFIX
-            })
 
-            newGuild.save()
-            .then(result => console.log(result))
-            .catch(err => console.error(err));
 
-            return message.channel.send('This server was not in our database! We have now added and you should be able to use bot commands.').then(m => m.delete({timeout: 10000}));
-        }
-    });
-
-    const prefix = settings.prefix;
+    const prefix = process.env.PREFIX;
 
     if (!message.guild) return;
-
-    if(message.content === `<@!${client.user.id}>` || message.content === `<@${client.user.id}>`) {
-        return message.channel.send(`Hey! my prefix in this guild is ${settings.prefix}`);
-    };
 	
 
 
